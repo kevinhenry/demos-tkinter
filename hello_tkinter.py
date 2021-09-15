b4bbb6337483e3bd910859b59c47aa688c2dff6f
Kevin Henry <kevinhenry@mac.com>
2021-09-14 19:24:41 -0700

    Working button demo

--- hello_tkinter.py
+++ hello_tkinter.py
@@ -1,6 +1,33 @@
-#!/usr/bin/python
+# #!/usr/bin/python
 
-import tkinter
-top = tkinter.Tk()
-# Code to add widgets will go here...
-top.mainloop()
+# import tkinter
+# top = tkinter.Tk()
+# # Code to add widgets will go here...
+# top.mainloop()
+
+
+# from tkinter import Tk, Label, Button
+from tkinter import *
+
+
+class MyFirstGUI:
+    def __init__(self, master):
+        self.master = master
+        master.title("A simple GUI")
+
+        self.label = Label(master, text="This is our first GUI!")
+        self.label.pack()
+
+        self.greet_button = Button(master, text="Greet", command=self.greet)
+        self.greet_button.pack()
+
+        self.close_button = Button(master, text="Close", command=master.quit)
+        self.close_button.pack()
+
+    def greet(self):
+        print("Greetings!")
+
+
+root = Tk()
+my_gui = MyFirstGUI(root)
+root.mainloop()
